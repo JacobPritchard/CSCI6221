f8bc4d629e4a01debb37026935cfeea08aa91f8a
JacobPritchard <jacobpritchard9@gmail.com>
2022-04-09 22:45:11 -0400

    Added Daily Forecast Page

--- src/DocumentEvent.js
+++ src/DocumentEvent.js
@@ -127,12 +127,10 @@ exports.getHourlyWeather = function(){
 		if(i % 3 == 0){
 			if(i != 0){
 				html += '</div>';
-				console.log("closed");
 				document.getElementById("hour").innerHTML += html;
 				html = "";
 			}
 			html += '<div class="row text-center">';
-			console.log("opened");
 		}
 		html += '<div class="col-md-4">';
 		var weather = hours[i].weather[0].description;
@@ -186,7 +184,6 @@ exports.getHourlyWeather = function(){
 	}
 	if(hours.length-1 % 3 != 0){
 		html += '</div>';
-		console.log("closed");
 	}
 	document.getElementById("hour").innerHTML += html;
   });
@@ -195,50 +192,142 @@ exports.getHourlyWeather = function(){
 exports.getDailyWeather = function(){
   $.get("https://api.openweathermap.org/data/2.5/onecall?lat=38.8950368&lon=-77.0365427&exclude=current,minutely,hourly,alerts&appid=883f70964bc4427b6582c086f8a59ff7&units=imperial", function(resp){
   	console.log(resp);
-	/*var weather = resp.current.weather[0].description;
-	var w = weather[0].toUpperCase();
-	for(var i = 1; i < weather.length; i++){
-		if(weather[i-1] == " "){
-			w += weather[i].toUpperCase();
-		}else{
-			w += weather[i];
-		}
-	}
-	var imgSrc = resp.current.weather[0].icon;
-	var src = "http://openweathermap.org/img/wn/" + imgSrc + ".png";
-	var img = '<span class="fa-stack fa-4x"><img width="150" height="150" src="'+src+'"/></span><h4 class="my-3">'+w+'</h4>';
-	var mins = resp.minutely;
+	var days = resp.daily;
 	var html = "";
-	for(var i = 0; i < mins.length; i++){
+	for(var i = 0; i < days.length; i++){
 		if(i % 3 == 0){
 			if(i != 0){
 				html += '</div>';
+				document.getElementById("day").innerHTML += html;
+				html = "";
 			}
 			html += '<div class="row text-center">';
 		}
 		html += '<div class="col-md-4">';
-		html += img;
-		var day = "";
-		var time = mins[i].dt;
-		var date = new Date(time * 1000);
-		var hours = date.getHours();
-		if(hours >= 12){
-			hours -= 12;
-			day = "pm";
-		}else{
-			day = "am";
+		var weather = days[i].weather[0].description;
+		var w = weather[0].toUpperCase();
+		for(var j = 1; j < weather.length; j++){
+			if(weather[j-1] == " "){
+				w += weather[j].toUpperCase();
+			}else{
+				w += weather[j];
+			}
 		}
-		var min = "0" + date.getMinutes();
-		var secs = "0" + date.getSeconds();
-		var t = hours + ':' + min.substr(-2) + ':' + secs.substr(-2);
-		html += '<p class="text-muted">' + t + ' ' + day + '</p>';
-		html += '<p class="text-muted">Precipitation: ' + mins[i].precipitation + ' mm</p>';
+		var imgSrc = days[i].weather[0].icon;
+		var src = "http://openweathermap.org/img/wn/" + imgSrc + ".png";
+		var img = '<span class="fa-stack fa-4x"><img width="150" height="150" src="'+src+'"/></span><h4 class="my-3">'+w+'</h4>';
+		html += img;
+		var time = days[i].dt;
+		var date = new Date(time * 1000);
+		var day = date.getDate();
+		var d2 = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+		var d1 = date.getDay();
+		var d = d2[d1];
+		var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+		var month = date.getMonth();
+		var m = months[month];
+		var year = date.getFullYear();
+		html += '<h4 class="my-3">' + d + ' ' + m + ' ' + day + ', ' + year + '</h4>';
+		html += '<p class="text-muted">Minimum Temperature: ' + days[i].temp.min + ' Degrees Fahrenheit</p>';
+		html += '<p class="text-muted">Maximum Temperature: ' + days[i].temp.max + ' Degrees Fahrenheit</p>';
+		html += '<p class="text-muted">Morning Temperature: ' + days[i].temp.morn + ' Degrees Fahrenheit</p>';
+		html += '<p class="text-muted">Daytime Temperature: ' + days[i].temp.day + ' Degrees Fahrenheit</p>';
+		html += '<p class="text-muted">Evening Temperature: ' + days[i].temp.eve + ' Degrees Fahrenheit</p>';
+		html += '<p class="text-muted">Nighttime Temperature: ' + days[i].temp.night + ' Degrees Fahrenheit</p>';
+		html += '<p class="text-muted">Morning Temperature Feels Like: ' + days[i].feels_like.morn + ' Degrees Fahrenheit</p>';
+		html += '<p class="text-muted">Daytime Temperature Feels Like: ' + days[i].feels_like.day + ' Degrees Fahrenheit</p>';
+		html += '<p class="text-muted">Evening Temperature Feels Like: ' + days[i].feels_like.eve + ' Degrees Fahrenheit</p>';
+		html += '<p class="text-muted">Nighttime Temperature Feels Like: ' + days[i].feels_like.night + ' Degrees Fahrenheit</p>';
+		var sunrise = days[i].sunrise;
+		var sunriseDate = new Date(sunrise * 1000);
+		var sunriseHours = sunriseDate.getHours();
+		var sunriseMins = "0" + sunriseDate.getMinutes();
+		var sunriseSecs = "0" + sunriseDate.getSeconds();
+		var sunriseTime = sunriseHours + ':' + sunriseMins.substr(-2) + ':' + sunriseSecs.substr(-2);
+		html += '<p class="text-muted">Sunrise: ' + sunriseTime + ' am</p>';
+		var sunset = days[i].sunset;
+		var sunsetDate = new Date(sunset * 1000);
+		var sunsetHours = sunsetDate.getHours()-12;
+		var sunsetMins = "0" + sunsetDate.getMinutes();
+		var sunsetSecs = "0" + sunsetDate.getSeconds();
+		var sunsetTime = sunsetHours + ':' + sunsetMins.substr(-2) + ':' + sunsetSecs.substr(-2);
+		html += '<p class="text-muted">Sunset: ' + sunsetTime + ' pm</p>';
+		var moonrise = days[i].moonrise;
+		var moonriseDate = new Date(moonrise * 1000);
+		var moonriseHours = moonriseDate.getHours();
+		var moonriseMins = "0" + moonriseDate.getMinutes();
+		var moonriseSecs = "0" + moonriseDate.getSeconds();
+		var amPm = "";
+		if(moonriseHours >= 12){
+			moonriseHours -= 12;
+			amPm = "pm";
+		}else{
+			amPm = "am";
+		}
+		var moonriseTime = moonriseHours + ':' + moonriseMins.substr(-2) + ':' + moonriseSecs.substr(-2);
+		html += '<p class="text-muted">Moonrise: ' + moonriseTime + ' ' + amPm + '</p>';
+		var moonset = days[i].moonset;
+		var moonsetDate = new Date(moonset * 1000);
+		var moonsetHours = moonsetDate.getHours();
+		var moonsetMins = "0" + moonsetDate.getMinutes();
+		var moonsetSecs = "0" + moonsetDate.getSeconds();
+		var amPm1 = "";
+		if(moonsetHours >= 12){
+			moonsetHours -= 12;
+			amPm1 = "pm";
+		}else{
+			amPm1 = "am";
+		}
+		var moonsetTime = moonsetHours + ':' + moonsetMins.substr(-2) + ':' + moonsetSecs.substr(-2);
+		html += '<p class="text-muted">Moonset: ' + moonsetTime + ' ' + amPm1 + '</p>';
+		var phase = days[i].moon_phase;
+		var moon = "";
+		if(phase == 0 || phase == 1){
+			moon = "New Moon";
+		}else if(phase > 0 && phase < 0.25){
+			moon = "Waxing Crescent Moon";
+		}else if(phase == 0.25){
+			moon = "First Quarter Moon";
+		}else if(phase > 0.25 && phase < 0.5){
+			moon = "Waxing Gibbous Moon";
+		}else if(phase == 0.5){
+			moon = "Full Moon";
+		}else if(phase > 0.5 && phase < 0.75){
+			moon = "Waning Gibbous Moon";
+		}else if(phase == 0.75){
+			moon = "Last Quarter Moon";
+		}else if(phase > 0.75 && phase < 1){
+			moon = "Waning Crescent Moon";
+		}else{
+			moon = "Error: Not a Part of the Lunar Cycle";
+		}
+		html += '<p class="text-muted">' + moon + '</p>';
+		var pop = days[i].pop;
+		var p = pop*100;
+		html += '<p class="text-muted">Precipitation Probability: ' + p + '%</p>';
+		html += '<p class="text-muted">Atmospheric Pressure: ' + days[i].pressure + ' hPa</p>';
+		html += '<p class="text-muted">Humidity: ' + days[i].humidity + '%</p>';
+		html += '<p class="text-muted">Atmospheric Temperature (Dew Point): ' + days[i].dew_point + ' Degrees Fahrenheit</p>';
+		html += '<p class="text-muted">Cloudiness: ' + days[i].clouds + '%</p>';
+		html += '<p class="text-muted">Midday UV Index: ' + days[i].uvi + '</p>';
+		html += '<p class="text-muted">Average Visibility: ' + days[i].visibility + ' m</p>';
+		html += '<p class="text-muted">Wind Speed: ' + days[i].wind_speed + ' miles/hour</p>';
+      	if(days[i].wind_gust){
+	  	  html += '<p class="text-muted">Wind Gust: ' + days[i].wind_gust + ' miles/hour</p>';
+		}
+		html += '<p class="text-muted">Wind Direction: ' + days[i].wind_deg + ' degrees (meteorological)</p>';
+		if(days[i].rain){
+		  html += '<p class="text-muted">Rain Volume: ' + days[i].rain + ' mm</p>';
+		}
+		if(days[i].snow){
+		  html += '<p class="text-muted">Snow Volume: ' + days[i].snow + ' mm</p>';
+		}
 		html += "</div>";
 	}
-	if(mins.length-1 % 3 != 0){
+	if(days.length-1 % 3 != 0){
 		html += '</div>';
 	}
-	document.getElementById("min").innerHTML = html;*/
+	document.getElementById("day").innerHTML += html;
   });
 }
 
